36a50f51da99a0bec552f1da2bd935eefc62aa2f
MaxQNEI <maxqnei@gmail.com>
2024-09-20 21:08:11 +0300

    esbuild-git-push

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,9 @@
 (() => {
-  // src/main.js
-  function loop() {
-    console.log("tick");
-  }
+    // src/main.js
+    function loop() {
+        console.log("tick");
+    }
 
-  // src/index.js
-  "Flag:".concat(loop.name);
+    // src/index.js
+    module.exports.loop = loop;
 })();
