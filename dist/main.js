69ec60a0af3f95ec6e261dd4aac537a6ee20da05
MaxQNEI <maxqnei@gmail.com>
2024-09-20 21:21:31 +0300

    esbuild-git-push

--- dist/main.js
+++ dist/main.js
@@ -11,8 +11,15 @@
         const spawn = Object.values(Game.spawns)[0];
         spawn.spawnCreep([WORK, CARRY, MOVE], "Harvester1");
       }
-      const Harvester1 = Game.creeps.Harvester1;
-      console.log(">", Harvester1);
+      const creep = Game.creeps.Harvester1;
+      if (!creep.memory.job) {
+        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+          if (!creep.memory.source) {
+            const sources = creep.room.find(FIND_SOURCES);
+            console.log(JSON.stringify(sources));
+          }
+        }
+      }
     }
   }
 })();
--- src/index.js
+++ src/index.js
@@ -13,8 +13,16 @@ export default function loop() {
             spawn.spawnCreep([WORK, CARRY, MOVE], "Harvester1");
         }
 
-        const Harvester1 = Game.creeps.Harvester1;
-        console.log(">", Harvester1);
+        const creep = Game.creeps.Harvester1;
+
+        if (!creep.memory.job) {
+            if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+                if (!creep.memory.source) {
+                    const sources = creep.room.find(FIND_SOURCES);
+                    console.log(JSON.stringify(sources));
+                }
+            }
+        }
     }
 
     // {
