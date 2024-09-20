0aa949bda5adb1d55ea9c26e7897490dc6b63d01
MaxQNEI <maxqnei@gmail.com>
2024-09-20 03:27:05 +0300

    auto-push

--- main/Rooms.js
+++ main/Rooms.js
@@ -1,3 +1,5 @@
+const XSource = require("./Source");
+
 class XRooms {
     every(fn) {
         for (const roomName in Game.rooms) {
@@ -10,6 +12,19 @@ class XRooms {
             for (const source of room.find(FIND_SOURCES)) {
                 fn(source, room);
             }
+        });
+    }
+
+    update() {
+        this.sources(function (source, room) {
+            const _source = new XSource(source);
+
+            room.memory.sources = room.memory.sources || {};
+            room.memory.sources[source.id] = {
+                limit: {
+                    creep: _source.creepLimit(),
+                },
+            };
         });
     }
 }
--- main/main.js
+++ main/main.js
@@ -1,18 +1,8 @@
 console.log("Load at", new Date().toString());
 
 const XRooms = require("./Rooms");
-const XSource = require("./Source");
 
 const _rooms = new XRooms();
-_rooms.sources(function (source, room) {
-    const _source = new XSource(source);
-
-    room.memory.sources = room.memory.sources || {};
-    room.memory.sources[source.id] = {
-        limit: {
-            creep: _source.creepLimit(),
-        },
-    };
-});
+_rooms.update();
 
 module.exports.loop = function loop() {};
