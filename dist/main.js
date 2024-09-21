7d951d37d9499a028b92dee687776de7040e5fb7
MaxQNEI <maxqnei@gmail.com>
2024-09-21 19:32:23 +0300

    <DARWIN/maxqnei> esbuild-git-push #6

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,60 @@
 (() => {
   // src/index.js
+  var CASES = {
+    HARVEST_TO_SPAWN: {
+      body: {
+        [BODY]: 1,
+        [CARRY]: 10,
+        [MOVE]: 10
+      }
+    },
+    HARVEST_TO_BUILD: {
+      body: {
+        [BODY]: 1,
+        [CARRY]: 10,
+        [MOVE]: 10
+      }
+    },
+    BUILD_THE_NEAREST: {
+      body: {
+        [BODY]: 1,
+        [CARRY]: 10,
+        [MOVE]: 10
+      }
+    }
+  };
+  function Unit(name = "Bunny", room, cases = []) {
+    if (!Game.creeps[name]) {
+      let spawnsInRoom = [];
+      let spawnMaxEnergy = 0;
+      for (const name2 in Game.spawns) {
+        const spawn2 = Game.spawns[name2];
+        if (spawn2.room === room) {
+          const structure = spawn2;
+          const energyUsed = spawn2.store.getUsedCapacity(RESOURCE_ENERGY);
+          const energyCapacity = spawn2.store.getCapacity(RESOURCE_ENERGY);
+          spawnMaxEnergy = Math.max(spawnMaxEnergy, energyCapacity);
+          spawnsInRoom.push({ structure, energyUsed, energyCapacity });
+        }
+      }
+      if (!spawn) {
+        return;
+      }
+      spawn.spawnCreep();
+      return;
+    }
+    if (Game.creeps[name].spawning) {
+      return;
+    }
+  }
   module.exports.loop = function loop() {
+    for (const name in Game.rooms) {
+      const room = Game.rooms[name];
+      Unit("Universal", room, [
+        CASES.HARVEST_TO_SPAWN,
+        CASES.HARVEST_TO_BUILD,
+        CASES.BUILD_THE_NEAREST
+      ]);
+    }
   }
 })();
--- src/index.js
+++ src/index.js
@@ -1,19 +1,88 @@
-// const CASES = {
-//     HARVEST_TO_SPAWN: "harvest to spawn",
-//     HARVEST_TO_BUILD: "harvest to build",
-//     BUILD_THE_NEAREST: "build the nearest",
-// };
+const CASES = {
+    HARVEST_TO_SPAWN: {
+        body: {
+            [BODY]: 1,
+            [CARRY]: 10,
+            [MOVE]: 10,
+        },
+    },
 
-// function Friend(cases = [HARVEST_TO_SPAWN]) {}
+    HARVEST_TO_BUILD: {
+        body: {
+            [BODY]: 1,
+            [CARRY]: 10,
+            [MOVE]: 10,
+        },
+    },
+
+    BUILD_THE_NEAREST: {
+        body: {
+            [BODY]: 1,
+            [CARRY]: 10,
+            [MOVE]: 10,
+        },
+    },
+};
+
+function Unit(name = "Bunny", room, cases = []) {
+    // const BODYPART_COST = {
+    //     tough: 10, // TOUGH
+    //     carry: 50, // CARRY
+    //     move: 50, // MOVE
+    //     attack: 80, // ATTACK
+    //     work: 100, // WORK
+    //     ranged_attack: 150, // RANGED_ATTACK
+    //     heal: 250, // HEAL
+    //     claim: 600, // CLAIM
+    // };
+
+    // Spawn if not exists
+    if (!Game.creeps[name]) {
+        // Find spawn
+        let spawnsInRoom = [];
+        let spawnMaxEnergy = 0;
+        for (const name in Game.spawns) {
+            const spawn = Game.spawns[name];
+
+            if (spawn.room === room) {
+                const structure = spawn;
+                const energyUsed = spawn.store.getUsedCapacity(RESOURCE_ENERGY);
+                const energyCapacity = spawn.store.getCapacity(RESOURCE_ENERGY);
+
+                spawnMaxEnergy = Math.max(spawnMaxEnergy, energyCapacity);
+
+                spawnsInRoom.push({ structure, energyUsed, energyCapacity });
+            }
+        }
+
+        // Spawn not exists
+        if (!spawn) {
+            return;
+        }
+
+        spawn.spawnCreep();
+
+        // Done
+        return;
+    }
+
+    // Break if spawning
+    if (Game.creeps[name].spawning) {
+        // Done
+        return;
+    }
+}
 
 export default function loop() {
-    // for spawn next friends
-    // Friend([HARVEST_TO_SPAWN]);
+    // Every room
+    for (const name in Game.rooms) {
+        const room = Game.rooms[name];
 
-    // universal Friend
-    // Friend([
-    //     CASES.HARVEST_TO_SPAWN,
-    //     CASES.HARVEST_TO_BUILD,
-    //     CASES.BUILD_THE_NEAREST,
-    // ]);
+        // create universal unit
+        Unit("Universal", room, [
+            CASES.HARVEST_TO_SPAWN,
+            CASES.HARVEST_TO_BUILD,
+            CASES.BUILD_THE_NEAREST,
+        ]);
+    }
 }
