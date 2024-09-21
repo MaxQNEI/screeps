28c45156264941ccbc00eb3bd8265049202eee98
MaxQNEI <maxqnei@gmail.com>
2024-09-21 18:37:30 +0300

    <DARWIN/maxqnei> esbuild-git-push #1

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,17 @@
 (() => {
   // src/index.js
+  var CASES = {
+    HARVEST_TO_SPAWN: "harvest to spawn",
+    HARVEST_TO_BUILD: "harvest to build",
+    BUILD_THE_NEAREST: "build the nearest"
+  };
+  function Friend(cases = [HARVEST_TO_SPAWN]) {
+  }
   module.exports.loop = function loop() {
+    Friend([
+      CASES.HARVEST_TO_SPAWN,
+      CASES.HARVEST_TO_BUILD,
+      CASES.BUILD_THE_NEAREST
+    ]);
   }
 })();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+const CASES = {
+    HARVEST_TO_SPAWN: "harvest to spawn",
+    HARVEST_TO_BUILD: "harvest to build",
+    BUILD_THE_NEAREST: "build the nearest",
+};
+
+function Friend(cases = [HARVEST_TO_SPAWN]) {}
+
 export default function loop() {
-    //
+    // for spawn next friends
+    // Friend([HARVEST_TO_SPAWN]);
+
+    // universal Friend
+    Friend([
+        CASES.HARVEST_TO_SPAWN,
+        CASES.HARVEST_TO_BUILD,
+        CASES.BUILD_THE_NEAREST,
+    ]);
 }
