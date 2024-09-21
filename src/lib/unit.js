fba857c5425cafb0f0d12a59847b67e876a1abed
MaxQNEI <maxqnei@gmail.com>
2024-09-21 18:20:58 +0300

    <DARWIN/maxqnei> esbuild-git-push #2

---
+++ src/lib/unit.js
@@ -0,0 +1,12 @@
+// BODYPART_COST: {
+//     "move": 50, // MOVE
+//     "work": 100, // WORK
+//     "attack": 80, // ATTACK
+//     "carry": 50, // CARRY
+//     "heal": 250, // HEAL
+//     "ranged_attack": 150, // RANGED_ATTACK
+//     "tough": 10, // TOUGH
+//     "claim": 600 // CLAIM
+// },
+
+export default function Unit(actions) {}
