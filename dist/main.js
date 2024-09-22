156bafe0efbc5d653865ea91aa9713f7b32b25c6
MaxQNEI <maxqnei@gmail.com>
2024-09-22 21:10:28 +0300

    <DARWIN/maxqnei> esbuild-git-push #31

--- dist/main.js
+++ dist/main.js
@@ -3,21 +3,22 @@
   function Unit(unit = { name: "Bunny", room: Room, body: [] }) {
     console.log(`Unit: ${unit.name}`);
     if (!Game.creeps[unit.name]) {
+      console.log("Cost...");
+      const cost = unit.body.reduce((pv, cv) => pv + BODYPART_COST[cv], 0);
+      console.log("Cost", cost);
       let spawnsInRoom = [];
       let spawnMaxEnergy = 0;
       for (const nameSpawn in Game.spawns) {
         const spawn2 = Game.spawns[nameSpawn];
-        if (spawn2.room === unit.room) {
-          const structure = spawn2;
-          const energyUsed = spawn2.store.getUsedCapacity(RESOURCE_ENERGY);
-          const energyCapacity = spawn2.store.getCapacity(RESOURCE_ENERGY);
+        const structure = spawn2;
+        const energyUsed = spawn2.store.getUsedCapacity(RESOURCE_ENERGY);
+        const energyCapacity = spawn2.store.getCapacity(RESOURCE_ENERGY);
+        if (spawn2.room === unit.room && energyUsed >= cost) {
           spawnMaxEnergy = Math.max(spawnMaxEnergy, energyCapacity);
           spawnsInRoom.push({ structure, energyUsed, energyCapacity });
         }
       }
-      console.log("Cost...");
-      const cost = unit.body.reduce((pv, cv) => pv + BODYPART_COST[cv], 0);
-      console.log("Cost", cost);
+      console.log(cost, spawnsInRoom[0]);
       return;
       if (!spawn) {
         return;
