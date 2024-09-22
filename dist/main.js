b583255fd0264876c197672f09238abe985fd1ec
MaxQNEI <maxqnei@gmail.com>
2024-09-22 20:48:00 +0300

    <DARWIN/maxqnei> esbuild-git-push #27

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,13 @@
 (() => {
   // src/index.js
-  function Unit(name = "Bunny", opts = { room: Room, body: [] }) {
+  function Unit(unit = { name: "Bunny", room: Room, body: [] }) {
     console.log(`Unit("${name}")`);
     if (!Game.creeps[name]) {
       let spawnsInRoom = [];
       let spawnMaxEnergy = 0;
       for (const name2 in Game.spawns) {
         const spawn2 = Game.spawns[name2];
-        if (spawn2.room === opts.room) {
+        if (spawn2.room === unit.room) {
           const structure = spawn2;
           const energyUsed = spawn2.store.getUsedCapacity(RESOURCE_ENERGY);
           const energyCapacity = spawn2.store.getCapacity(RESOURCE_ENERGY);
@@ -16,7 +16,7 @@
         }
       }
       console.log("Cost...");
-      const cost = opts.body.reduce((pv, cv) => pv + BODYPART_COST[cv], 0);
+      const cost = unit.body.reduce((pv, cv) => pv + BODYPART_COST[cv], 0);
       console.log("Cost", cost);
       return;
       if (!spawn) {
@@ -30,10 +30,10 @@
     }
   }
   module.exports.loop = function loop() {
-    for (const name in Game.rooms) {
-      const room = Game.rooms[name];
-      Unit("Universal", {
-        room,
+    for (const name2 in Game.rooms) {
+      Unit({
+        name: "Universal",
+        room: Game.rooms[name2],
         body: [WORK, CARRY, MOVE]
       });
     }
