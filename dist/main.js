3fdf7ca9ecae89d05e3fd5848da9519c7aa24302
MaxQNEI <maxqnei@gmail.com>
2024-09-22 21:32:49 +0300

    <DARWIN/maxqnei> esbuild-git-push #38

--- dist/main.js
+++ dist/main.js
@@ -1,44 +1,13 @@
 (() => {
-  // lib/sort.js
-  function asc(a, b) {
-    return a === b ? 0 : a > b ? 1 : -1;
-  }
-
-  // src/lib/SpawnCreep.js
-  function SpawnCreep(unit = { name: "Bunny", room: Room, body: [] }) {
-    console.log(`SpawnCreep: ${unit.name}`);
-    if (!Game.creeps[unit.name]) {
-      console.log("Cost...");
-      const cost = unit.body.reduce((pv, cv) => pv + BODYPART_COST[cv], 0);
-      console.log("Cost", cost);
-      let spawnsInRoom = [];
-      let spawnMaxEnergy = 0;
-      for (const nameSpawn in Game.spawns) {
-        const spawn2 = Game.spawns[nameSpawn];
-        const structure = spawn2;
-        const energyUsed = spawn2.store.getUsedCapacity(RESOURCE_ENERGY);
-        const energyCapacity = spawn2.store.getCapacity(RESOURCE_ENERGY);
-        if (spawn2.room === unit.room && energyUsed >= cost) {
-          spawnMaxEnergy = Math.max(spawnMaxEnergy, energyCapacity);
-          spawnsInRoom.push({ structure, energyUsed, energyCapacity });
-        }
-      }
-      const spawn = spawnsInRoom?.[0]?.structure;
-      if (!spawn) {
-        return;
-      }
-      spawn.spawnCreep(unit.body.sort(asc), unit.name);
-      return;
-    }
-    if (Game.creeps[unit.name].spawning) {
-      return;
-    }
+  // src/lib/creep/Creep.fn.js
+  function Creep(creep) {
+    return {};
   }
 
   // src/index.js
   module.exports.loop = function loop() {
     for (const nameRoom in Game.rooms) {
-      SpawnCreep({
+      Creep({
         name: "Universal",
         room: Game.rooms[nameRoom],
         body: [WORK, CARRY, MOVE]
