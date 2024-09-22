2cdb5c04be40980f5f3a2e26df943efb425244af
MaxQNEI <maxqnei@gmail.com>
2024-09-22 22:22:16 +0300

    <DARWIN/maxqnei> esbuild-git-push #72

--- dist/main.js
+++ dist/main.js
@@ -55,6 +55,7 @@
       if (findType === FIND_SPAWN_TO_SPAWN_CREEP_BY_COST) {
         for (const nameSpawn in Game.spawns) {
           const spawn = Game.spawns[nameSpawn];
+          console.log(spawn.store[RESOURCE_ENERGY], Foptions.cost);
           if (spawn.room === _room && spawn.store[RESOURCE_ENERGY] >= options.cost) {
             return spawn;
           }
